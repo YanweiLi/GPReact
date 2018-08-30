06df268c389ff702366225ae978f5921c4d60bb1
Liyanwei <yifan.lyw@alibaba-inc.com>
2018-08-30 19:30:11 +0800

    新建工程

---
+++ GPReactDemo/GPReactDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPReactDemo
+//
+//  Created by Liyanwei on 2018/8/30.
+//  Copyright © 2018年 Liyanwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GPReactDemo/GPReactDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPReactDemo
+//
+//  Created by Liyanwei on 2018/8/30.
+//  Copyright © 2018年 Liyanwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
